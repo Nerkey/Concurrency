1b694d62d0e348395d6a9a6cae2c0f6033ce4bfd
曲宇杰 <ququ1120@126.com>
2017-09-27 15:37:19 +0800

    线程的中断

---
+++ src/main/java/com/qu/PrimeGenerator.java
@@ -0,0 +1,48 @@
+package com.qu;
+
+public class PrimeGenerator extends Thread {
+
+    @Override
+    public void run() {
+        long number = 1L;
+        while (true) {
+            if (isPrime(number)) {
+                System.out.printf("Number %d is Prime\n", number);
+            }
+            interrupted();
+            if (isInterrupted()) {
+                System.out.printf("Thr Prime Generator has been Interrupted");
+                return;
+            }
+            number++;
+        }
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 2) {
+            return true;
+        }
+
+        for (long i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Thread task = new PrimeGenerator();
+        task.start();
+
+        try {
+            Thread.sleep(5000);
+            task.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
