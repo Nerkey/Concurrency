46f45102ab5e4e0a3dc5982c16320f7ba79df7d6
曲宇杰 <ququ1120@126.com>
2017-09-28 15:35:02 +0800

    等待线程的终结

---
+++ src/main/java/com/qu/DataSourcesLoader.java
@@ -0,0 +1,32 @@
+package com.qu;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class DataSourcesLoader implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.printf("Beginning data sources loading: %s \n", new Date());
+    }
+
+    public static void main(String[] args) {
+        DataSourcesLoader dsLoader = new DataSourcesLoader();
+        Thread thread1 = new Thread(dsLoader, "DataSourceThread");
+        NetworkConnectionsLoader ncLoader = new NetworkConnectionsLoader();
+        Thread thread2 = new Thread(ncLoader, "NetworkConnectionLoader");
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("Main: Configuration has been loaded: %s\n", new Date());
+    }
+
+}
---
+++ src/main/java/com/qu/NetworkConnectionsLoader.java
@@ -0,0 +1,19 @@
+package com.qu;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class NetworkConnectionsLoader implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(6);
+        } catch (InterruptedException e) {
+            System.out.printf("Connections Interrupted");
+        }
+
+        System.out.printf("Beginning network connections loading: %s \n", new Date());
+    }
+
+}
