30d0d0d1aea17128a61bc39c02177aef4b75a9fe
曲宇杰 <ququ1120@126.com>
2017-09-28 17:33:03 +0800

    守护线程的创建和运行

---
+++ src/main/java/com/qu/WriterTask.java
@@ -0,0 +1,61 @@
+package com.qu;
+
+import java.util.ArrayDeque;
+import java.util.Date;
+import java.util.Deque;
+import java.util.concurrent.TimeUnit;
+
+public class WriterTask implements Runnable {
+
+    private Deque<Event> deque;
+
+    public WriterTask(Deque<Event> deque) {
+        this.deque = deque;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 100; i++) {
+            Event event = new Event();
+            event.setDate(new Date());
+            event.setEvent(String.format("The thread %s has generated an event", Thread.currentThread().getId()));
+            deque.addFirst(event);
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Deque<Event> deque = new ArrayDeque<>();
+        WriterTask writer = new WriterTask(deque);
+
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(writer);
+            thread.start();
+        }
+
+        CleanerTask cleaner = new CleanerTask(deque);
+        cleaner.start();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
