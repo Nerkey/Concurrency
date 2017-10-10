617afb5495dc0956fcb8dc12d0846154b958a56e
曲宇杰 <ququ1120@126.com>
2017-10-10 14:58:36 +0800

    处理线程组内的不受控制异常

---
+++ src/main/java/com/qu/MyThreadGroup.java
@@ -0,0 +1,16 @@
+package com.qu;
+
+public class MyThreadGroup extends ThreadGroup {
+
+    public MyThreadGroup(String name) {
+        super(name);
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.printf("Thre thread %s has thrown an Exception \n", t.getId());
+        e.printStackTrace();
+        System.out.printf("Terminating the rest of the Threads\n");
+        interrupt();
+    }
+}
