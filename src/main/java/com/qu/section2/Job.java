216e5c3c5506ca5af08a4d98aea3cae864f594b9
曲宇杰 <ququ1120@126.com>
2017-10-12 10:50:56 +0800

    使用Lock同步代码块

---
+++ src/main/java/com/qu/section2/Job.java
@@ -0,0 +1,32 @@
+package com.qu.section2;
+
+import java.util.Arrays;
+
+public class Job implements Runnable {
+
+    private PrintQueue printQueue;
+
+    public Job(PrintQueue printQueue) {
+        this.printQueue = printQueue;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
+        printQueue.printJob(new Object());
+        System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread[] threads = new Thread[10];
+
+        for (int i=0; i<10; i++){
+            threads[i]=new Thread(new Job(printQueue),"Thread "+ i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+    }
+}
